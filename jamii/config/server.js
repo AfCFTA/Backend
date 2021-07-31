138a9b7db284709abdceadc824f121442c3fa8ae
KingMabuza <amal@amallevi.com>
2021-07-31 12:57:52 +0200

    first commit

---
+++ jamii/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e3720fd13247b96be041698e960af8f5'),
+    },
+  },
+});
---
+++ jamii/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ac34a54-87ba-4c9e-9e69-0a774370b316'
+};
